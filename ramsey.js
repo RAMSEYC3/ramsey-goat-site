f9af02d5b65d579f57095351c42dd5ae46378d49
RAMSEYC3 <cedouramsey11@gmail.com>
2024-05-30 11:44:48 +0200

    ramsey

---
+++ ramsey.js
@@ -0,0 +1,28 @@
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: 'fade',
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     rewind: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
